6d1c5c31ee68c5b820a95ec08c6cf247a55c1054
kylefarmer85 <kylefarmer85@gmail.com>
2020-10-28 20:17:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,92 @@
-// Add your code here
+submitData()
+
+function submitData() {
+  const form = document.getElementById('user-form')
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault()
+
+    const formData = {
+     name: e.target['name'].value,
+     email: e.target['email'].value
+    }
+    e.target.reset()  
+    
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    }
+ 
+  return fetch('http://localhost:3000/users', configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    })
+    .catch(function(error) {
+      alert("Error!!");
+      console.log(error.message);
+    });
+  })
+}
+
+
+
+
+function submitData( name, email ) {
+  return fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name,
+        email
+      } )
+    } )
+    .then( function ( response ) {
+      return response.json()
+    } )
+    .then( function ( object ) {
+      document.body.innerHTML = object[ "id" ]
+    } )
+    .catch( function ( error ) {
+      document.body.innerHTML = error.message
+    } )
+}
+
+
+
+
+
+// let formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle"
+// };
+
+// let configObj = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify(formData)
+// };
+
+// fetch("http://localhost:3000/dogs", configObj)
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(object) {
+//     console.log(object);
+//   });
+//   .catch(function(error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   });
